10ab0bbf40e528c44ed555abd438bf806ea37d92
Hershit-shukla <109151752+Hershit-shukla@users.noreply.github.com>
2023-04-15 02:06:11 +0530

    githeat.sh

---
+++ GitHeat/githeat.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Text styles
+BOLD=$(tput bold)
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+RESET=$(tput sgr0)
+
+# Prompt user for input
+echo "${BOLD}Enter the remote URL of the GitHub repository:${RESET}"
+read git_remote
+
+echo "${BOLD}Enter the number of days to go back from today:${RESET}"
+read days_backwards
+
+echo "${BOLD}Enter the name of the subdirectory to create the repository in:${RESET}"
+read repository_name
+
+# Initialize repository
+echo "${BOLD}[1/3] Initializing repository...${RESET}"
+bash initialize-repository.sh $repository_name 1>/dev/null
+if [ $? -ne 0 ]; then
+    echo "${RED}⨯ Initializing repository failed!${RESET}"
+    exit 1
+else
+    echo "${GREEN}✓ Repository successfully initialized!${RESET}"
+fi
+
+# Create commits
+echo "${BOLD}[2/3] Creating commits (this can take a few seconds)...${RESET}"
+bash create-commits.sh $repository_name $days_backwards 1>/dev/null
+if [ $? -ne 0 ]; then
+    echo "${RED}⨯ Creating commits failed!${RESET}"
+    exit 1
+else
+    echo "${GREEN}✓ Commits successfully created!${RESET}"
+fi
+
+# Push repository to remote
+echo "${BOLD}[3/3] Pushing repository to remote...${RESET}"
+bash push-to-github.sh $repository_name $git_remote 1>/dev/null
+if [ $? -ne 0 ]; then
+    echo "${RED}⨯ Pushing repository to remote failed!${RESET}"
+    exit 1
+else
+    echo "${GREEN}✓ Repository successfully pushed to remote!${RESET}"
+fi
